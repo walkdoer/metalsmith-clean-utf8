a38d33abcde9383d4780a8f18e8f1a3e90a5b4ed
zhangmh <zhangmhao@gmail.com>
2017-03-02 18:34:25 +0800

    first version

---
+++ README.md
@@ -0,0 +1,21 @@
+i
+# metalsmith-markdown
+
+  A Metalsmith plugin to clean utf8 string
+
+## Installation
+
+    $ npm install metalsmith-clean-utf8
+
+
+## Javascript Usage
+
+```js
+var cleanUtf8 = require('metalsmith-clean-utf8');
+
+metalsmith.use(cleanUtf8());
+```
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,43 @@
+var extname = require('path').extname;
+var debug = require('debug')('clean-utf8');
+
+/**
+ * Expose `plugin`.
+ */
+
+module.exports = plugin;
+
+/**
+ * Metalsmith plugin to clean utf8
+ *
+ * @return {Function}
+ */
+
+function plugin(options){
+  options = options || {};
+
+  return function(files, metalsmith, done){
+    Object.keys(files).forEach(function(file){
+      debug('checking file: %s', file);
+      console.log(file);
+      if (!markdown(file)) return;
+      var data = files[file];
+      var str = data.contents.toString();
+      str = str.replace(/[\x00-\x1F\x7F-\x9F]/ug, '');
+      data.contents = new Buffer(str);
+      
+      done();
+    });
+  };
+}
+
+/**
+ * Check if a `file` is markdown.
+ *
+ * @param {String} file
+ * @return {Boolean}
+ */
+
+function markdown(file){
+  return /\.md|\.markdown/.test(extname(file));
+}
